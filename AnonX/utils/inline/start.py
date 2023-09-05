b8c6b91430b728651deb4ceac0143f48afc13547
Almortagel <105941198+Almortage@users.noreply.github.com>
2023-09-05 23:49:51 +0300

    Add files via upload

--- AnonX/utils/inline/start.py
+++ AnonX/utils/inline/start.py
@@ -37,9 +37,6 @@ def private_panel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
         [
             InlineKeyboardButton(
                 text="🗒 الاوامر", callback_data="settings_back_helper"
-            ),
-            InlineKeyboardButton(
-                text="المساعدة", callback_data="settings_helper"
             )
         ],
         [
